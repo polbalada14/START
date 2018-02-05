4fc97e9b8f272847d262eec2091031d9625dc73b
polbalada14 <36170280+polbalada14@users.noreply.github.com>
2018-02-05 20:38:33 +0100

    primer.py

---
+++ .gitignore/.py
@@ -0,0 +1,12 @@
+#coding :utf8
+
+nombre=raw_input(Indica el teu nom: ")
+edat=input("Indica la teva edat: ")
+
+#Si es major de 18 anys, pots entrar a la disco
+If edat>=18
+print "Major d'edat, pots entrar"
+
+#Si es menor d'edat no pot entrar a la festa
+else edat<18
+print "ho sento",nombre,"ets menor d'edat, de fiesta al chiciparck"
